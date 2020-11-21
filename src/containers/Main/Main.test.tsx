72cee312820e217ad50ce44c2387064e68712da4
brunubarbosa <brunopjsw@gmail.com>
2020-11-21 04:11:28 -0300

    TDD/ add form new item assert

--- src/containers/Main/Main.test.tsx
+++ src/containers/Main/Main.test.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import {render, fireEvent} from '@testing-library/react';
+import {
+  render,
+  fireEvent,
+  getByPlaceholderText,
+  getByLabelText,
+} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import Main from './index';
 
 const mockfc = jest.fn();
 
-describe('The button', () => {
+describe('Test Main component empty state', () => {
   it('Should find image logo', () => {
     const {getByAltText} = render(<Main />);
     expect(getByAltText('logo')).toBeTruthy();
@@ -28,5 +33,30 @@ describe('The button', () => {
       target: {value: searchValue},
     });
     expect(searchInput).toHaveValue(searchValue);
+  });
+});
+describe('Test main component add item action', () => {
+  it('Should find add item form', () => {
+    const {getByDisplayValue, getByText} = render(<Main />);
+    fireEvent.click(getByText('Criar contato'));
+    expect(getByDisplayValue('name')).toBeTruthy();
+    expect(getByDisplayValue('email')).toBeTruthy();
+    expect(getByDisplayValue('tel')).toBeTruthy();
+  });
+  it('Should find save button disabled with no data filled', () => {
+    const {getByText} = render(<Main />);
+    fireEvent.click(getByText('Criar contato'));
+    const saveButton = getByText('Salvar');
+    expect(saveButton).toBeDisabled();
+  });
+  it('Should ', () => {
+    const {getByText, getByLabelText} = render(<Main />);
+    fireEvent.click(getByText('Criar contato'));
+    const saveButton = getByText('Salvar');
+    const nameValue = 'Maria';
+    fireEvent.change(getByLabelText('Nome'), {
+      target: {value: nameValue},
+    });
+    expect(saveButton).not.toBeDisabled();
   });
 });
