3cf8aeb6ceb62604a6ee2bb0ebbe2757c2560243
brunubarbosa <brunopjsw@gmail.com>
2020-11-21 10:00:50 -0300

    basic settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
-import Button from './components/Button';
 import Main from './containers/Main';
 
 const App: React.FunctionComponent<{}> = () => {
