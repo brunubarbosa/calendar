adefebefb2ed129a603610afd0dffaf12150060c
brunubarbosa <brunopjsw@gmail.com>
2020-11-24 15:39:46 -0300

    form update

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -28,16 +28,24 @@ const ContactForm: React.FunctionComponent<ContactFormProps> = ({
   return (
     <div className={styles.wrapper}>
       <Modal isOpen={isOpen} onClose={onCloseModal} clickOutsideToClose={true}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <h1>Criar novo contato</h1>
-          <label htmlFor="name">Nome</label>
-          <input ref={register} type="text" name="name" id="name" />
-          <label htmlFor="email">E-mail</label>
-          <input ref={register} type="text" name="email" id="email" />
-          <label htmlFor="tel">Telefone</label>
-          <input ref={register} type="text" name="tel" id="tel" />
-          <Button text="Cancelar" kind="secondary" />
-          <Button disabled={!formState.isDirty} text="Salvar" type="submit" />
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+          <h1 className={styles.title}>Criar novo contato</h1>
+          <div className={styles.fieldWrapper}>
+            <label htmlFor="name">Nome</label>
+            <input ref={register} type="text" name="name" id="name" />
+          </div>
+          <div className={styles.fieldWrapper}>
+            <label htmlFor="email">E-mail</label>
+            <input ref={register} type="text" name="email" id="email" />
+          </div>
+          <div className={`${styles.fieldWrapper} ${styles.tel}`}>
+            <label htmlFor="tel">Telefone</label>
+            <input ref={register} type="text" name="tel" id="tel" />
+          </div>
+          <footer className={styles.footer}>
+            <Button text="Cancelar" kind="secondary" />
+            <Button disabled={!formState.isDirty} text="Salvar" type="submit" />
+          </footer>
         </form>
       </Modal>
     </div>
